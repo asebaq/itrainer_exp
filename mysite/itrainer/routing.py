70ee1074b7b69f8883f1d8ad75022ded60f16ccd
asebaq <ahmad.sebaq@gmail.com>
2024-02-06 20:51:21 +0200

    Fix routing

--- mysite/itrainer/routing.py
+++ mysite/itrainer/routing.py
@@ -1,16 +1,7 @@
-# your_app/routing.py
-
 from django.urls import path
-from .consumers import WebcamConsumer
 
-from channels.routing import ProtocolTypeRouter, URLRouter
-application = ProtocolTypeRouter({
-    "websocket": URLRouter([
-        path("ws/webcam/", WebcamConsumer.as_asgi()),
-    ]),
-})
+from . import consumers
 
-# websocket_urlpatterns = [
-#     path("ws/webcam/", WebcamConsumer.as_asgi()),
-# ]
-
+websocket_urlpatterns = [
+    path("ws/webcam/", consumers.WebcamConsumer.as_asgi()),
+]
