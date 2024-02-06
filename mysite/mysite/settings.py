cb753b6f2673f0dbdbeacd942a65986917513dc8
asebaq <ahmad.sebaq@gmail.com>
2024-02-06 21:42:38 +0200

    Allow all host

--- mysite/mysite/settings.py
+++ mysite/mysite/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-i_^8@i+3fqflekfkmkd3-@!^ohlbkaot8oe4hhs%ih^zf+^5@a
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
